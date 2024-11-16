ba202db5b80913c04de9939e8f3aeae95e3611ff
Heloisa <sousaheloisa35@gmail.com>
2024-11-16 20:49:59 -0300

    projetos ok!

---
+++ script.js
@@ -0,0 +1,12 @@
+// Script para animação suave de rolagem para links internos
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+
+        });
+    });
+
+});
